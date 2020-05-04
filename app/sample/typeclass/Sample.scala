3aeba2b67f92d6acd0c93ed0b87af5d8608e6cf1
Keyno <kofujiw@gmail.com>
2020-05-04 22:25:06 +0900

    update type annotation.

--- app/sample/typeclass/Sample.scala
+++ app/sample/typeclass/Sample.scala
@@ -6,11 +6,11 @@ object Sample extends App {
     def convert(value: T) : String
   }
 
-  implicit val str = new Convert[String] {
+  implicit val str: Convert[String] = new Convert[String] {
     override def convert(value: String): String = s"convert string ${value}"
   }
 
-  implicit val int = new Convert[Int] {
+  implicit val int: Convert[Int] = new Convert[Int] {
     override def convert(value: Int): String = s"convert int ${value.toString}"
   }
 
@@ -20,7 +20,7 @@ object Sample extends App {
   convert(100)
   convert("Hello")
 
-  implicit val int1 = new Convert[Int] {
+  implicit val int1: Convert[Int] = new Convert[Int] {
     override def convert(value: Int): String = s"convert int1 ${value.toString}"
   }
   convert(200)(int)
