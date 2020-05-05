1c4008dac60994e564bfc89a31d6146a41be2104
Keyno <kofujiw@gmail.com>
2020-05-06 04:00:00 +0900

    Repository, Service に分割

---
+++ app/repository/MonsterRepository.scala
@@ -0,0 +1,36 @@
+package repository
+
+import javax.inject._
+import scalikejdbc._
+
+@Singleton
+class MonsterRepository {
+  /*
+    // initialize JDBC driver & connection pool
+    Class.forName("org.postgresql.Driver")
+
+    val url = "jdbc:postgresql://localhost:45432/sample_db"
+    val user = "fujiwara"
+    val pass = "fujiwara"
+    ConnectionPool.singleton(url, user, pass)
+
+    // ad-hoc session provider on the REPL
+    //implicit val session = AutoSession
+ */
+
+  def getAll(): List[String] = {
+    DB readOnly { implicit session =>
+      sql"select name from m_monster".map(_.string("name")).list.apply()
+    }
+  }
+
+  def get(number: Option[String]): List[String] = {
+    val numberVal = number.getOrElse("")
+    DB readOnly { implicit session =>
+      sql"select name from m_monster where id = ${numberVal}".map(_.string("name")).list.apply()
+    }
+
+  }
+
+
+}
--- app/services/MonsterService.scala
+++ app/services/MonsterService.scala
@@ -1,36 +1,16 @@
 package services
 
-import javax.inject.Singleton
-import scalikejdbc._
+import javax.inject.{Inject, Singleton}
+import repository.MonsterRepository
 
 @Singleton
-class MonsterService {
-  /*
-  // initialize JDBC driver & connection pool
-  Class.forName("org.postgresql.Driver")
-
-  val url = "jdbc:postgresql://localhost:45432/sample_db"
-  val user = "fujiwara"
-  val pass = "fujiwara"
-  ConnectionPool.singleton(url, user, pass)
-
-  // ad-hoc session provider on the REPL
-  //implicit val session = AutoSession
-   */
+class MonsterService @Inject() (mr: MonsterRepository) {
 
   def getAll() = {
-    val names = DB readOnly { implicit session =>
-      sql"select name from m_monster".map(_.string("name")).list.apply()
-    }
-    names
+    mr.getAll()
   }
 
   def get(number: Option[String]) = {
-    val numberVal = number.getOrElse("")
-    val name = DB readOnly { implicit session =>
-      sql"select name from m_monster where id = ${numberVal}".map(_.string("name")).list.apply()
-    }
-    name
+    mr.get(number)
   }
-
 }
